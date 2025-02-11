0035320cc647b7a889bf1e196ff88eb4a7f34453
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-11 11:18:58 +0100

    :tada:

--- apps/nextjs/src/app/[domain]/third-term/page.tsx
+++ apps/nextjs/src/app/[domain]/third-term/page.tsx
@@ -1,13 +1,3 @@
-import { prisma } from "@acme/db";
-
 export default async function Page({}) {
-  const r = await prisma.subjects.findMany({});
-
-  return (
-    <div>
-      {r.map((a) => (
-        <div>{a.name}</div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
