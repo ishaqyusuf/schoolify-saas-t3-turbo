25a87ffe07e3d20caf6bd51aa59186eb6391a568
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-11 11:43:48 +0100

    :tada:

--- apps/nextjs/src/lib/prisma.ts
+++ apps/nextjs/src/lib/prisma.ts
@@ -1,6 +1,6 @@
-// import { PrismaClient } from "@acme/db";
+import { PrismaClient } from "@acme/db";
 import { toSentenceCase } from "@acme/utils";
 
 export const prisma = {} as any;
 
-export const pClient = toSentenceCase("");
+export const pClient = PrismaClient;
