8bdb623153c49ee4a4e3cf114bb64ddc7f0d0f5d
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-12 09:26:20 +0100

    :tada:

--- apps/nextjs/src/app/third-term/layout.tsx
+++ apps/nextjs/src/app/third-term/layout.tsx
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
+
 import ThirdTermHeader from "~/components/third-term-header";
 
 export default async function Layout({ children }) {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <ThirdTermHeader />
       {children}
-    </div>
+    </Suspense>
   );
 }
