78e34b2671d9d7fd19a6025b4aae7f3fbb15b89f
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-22 18:14:50 +0100

    :tada:

--- apps/nextjs/src/components/result-class-list.tsx
+++ apps/nextjs/src/components/result-class-list.tsx
@@ -63,7 +63,7 @@ export default function ResultClassList({
         <div className="">{data.classTitle}</div>
       </CollapsibleTrigger>
       <CollapsibleContent className="w-screen overflow-auto sm:px-8">
-        <Select defaultValue={subjectCode} onValueChange={setSubjectCode}>
+        {/* <Select defaultValue={subjectCode} onValueChange={setSubjectCode}>
           <SelectTrigger className="w-[280px]">
             <SelectValue placeholder="" />
           </SelectTrigger>
@@ -74,79 +74,81 @@ export default function ResultClassList({
               </SelectItem>
             ))}
           </SelectContent>
-        </Select>
-        <div className="w-screen overflow-x-auto">
-          <Table
-            dir="rtl"
-            className="table-fixeds w-screen table-auto sm:w-full"
-          >
-            <TableHeader>
-              <TableRow>
-                <TableHead className="sticky left-0 z-10 w-48 bg-white">
-                  Name
-                </TableHead>
-                {data.subjects?.map((s) => (
-                  <TableHead
-                    className={cn(
-                      s.assessments?.length == 1 && "",
-                      s.assessments?.length == 2 && "",
-                      s.assessments?.length == 3 && "w-36 border",
-                    )}
-                    align="center"
-                    colSpan={s.assessments?.length || 1}
-                    key={s.id}
-                  >
-                    <span className="text-center">
-                      {s.classRoomSubject?.subject?.title}
-                    </span>
+        </Select> */}
+        <div className="w-screen overflow-hidden">
+          <div className="relative w-full overflow-x-scroll">
+            <Table dir="rtl" className="">
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="sw-48 sticky right-0 z-10 bg-white">
+                    Name
                   </TableHead>
-                ))}
-              </TableRow>
-              <TableRow>
-                <TableHead className="p-2"></TableHead>
-                {data.subjects
-                  .map((s) => s.assessments || [{} as any])
-                  .flat()
-                  ?.map((s) => (
-                    <TableHead className="border p-2" key={s.id}>
-                      <div>{assessmentShortTitle(s.title)}</div>
+                  {data.subjects?.map((s) => (
+                    <TableHead
+                      className={cn(
+                        s.assessments?.length == 1 && "",
+                        s.assessments?.length == 2 && "",
+                        s.assessments?.length == 3 && "w-36 border",
+                      )}
+                      align="center"
+                      colSpan={s.assessments?.length || 1}
+                      key={s.id}
+                    >
+                      <span className="text-center">
+                        {s.classRoomSubject?.subject?.title}
+                      </span>
                     </TableHead>
                   ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data.students?.map((s) => (
-                <TableRow
-                  onClick={() => {
-                    openStudentSubjectForm(s.id);
-                  }}
-                  key={s.id}
-                >
-                  <TableCell className="sticky left-0 z-10 whitespace-nowrap bg-white">
-                    {`${s.firstName} ${s.fathersName} ${s.otherName || ""}`}
-                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2"></TableHead>
                   {data.subjects
-                    // .map((s) => s.assessments || [{} as any])
-                    // .flat()
-                    ?.map((sub) => (
-                      <Fragment key={sub.id}>
-                        {(
-                          (sub.assessments.length
-                            ? sub.assessments
-                            : ([{}] as any)) as typeof sub.assessments
-                        ).map((res) => (
-                          <ResultCell
-                            assessment={res}
-                            student={s}
-                            key={res.id}
-                          ></ResultCell>
-                        ))}
-                      </Fragment>
+                    .map((s) => s.assessments || [{} as any])
+                    .flat()
+                    ?.map((s) => (
+                      <TableHead className="border p-2" key={s.id}>
+                        {/* <div>{assessmentShortTitle(s.title)}</div> */}
+                        <div className="line-clamp-1">{s.title}</div>
+                      </TableHead>
                     ))}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody>
+                {data.students?.map((s, i) => (
+                  <TableRow
+                    onClick={() => {
+                      openStudentSubjectForm(s.id);
+                    }}
+                    className={cn(i % 2 == 0 && "bg-muted")}
+                    key={s.id}
+                  >
+                    <TableCell className="sticky right-0 z-10 whitespace-nowrap bg-white p-2">
+                      {`${enToAr(i + 1)}. `}
+                      {`${s.firstName} ${s.fathersName} ${s.otherName || ""}`}
+                    </TableCell>
+                    {data.subjects
+                      // .map((s) => s.assessments || [{} as any])
+                      // .flat()
+                      ?.map((sub) => (
+                        <Fragment key={sub.id}>
+                          {(
+                            (sub.assessments.length
+                              ? sub.assessments
+                              : ([{}] as any)) as typeof sub.assessments
+                          ).map((res) => (
+                            <ResultCell
+                              assessment={res}
+                              student={s}
+                              key={res.id}
+                            ></ResultCell>
+                          ))}
+                        </Fragment>
+                      ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       </CollapsibleContent>
     </Collapsible>
